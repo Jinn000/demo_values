9796c7fd8d0a16ed4560909edc6c288014ff0804
Андрей Задорожний <azadorozhnyj@msc.sigma-it.local>
2020-11-26 10:37:43 +0300

    start

---
+++ src/main/java/ru/zav/demo_values/DemoValuesApplication.java
@@ -0,0 +1,13 @@
+package ru.zav.demo_values;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoValuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoValuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zav/demo_values/DemoValuesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zav.demo_values;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoValuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
